97fd0e9ef3599abea44fcee7284cc1c6b661fbfd
sbx_user1051 <sbx_user1051@169.254.7.21>
2020-11-08 17:19:44 +0000

    Sun Nov 08 17:19:44 UTC 2020

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -2,6 +2,8 @@ package befaster.solutions.SUM;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -11,12 +13,18 @@ public class SumSolutionTest {
 
     @Before
     public void setUp() {
-
         sum = new SumSolution();
     }
 
     @Test
-    public void compute_sum() {
+    public void compute_sum_1_1() {
         assertThat(sum.compute(1, 1), equalTo(2));
     }
+
+    @Test
+    @Parameters
+    public void compute_sum_0_0() {
+
+    }
 }
+
