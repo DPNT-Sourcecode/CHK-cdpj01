1bd8bb8dd474a93d9dae296c54fe9f2573eae4b4
sbx_user1051 <sbx_user1051@169.254.16.73>
2020-11-15 16:07:48 +0000

    Sun Nov 15 16:07:48 UTC 2020

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -8,9 +8,10 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class HelloSolutionTest {
 
+    private HelloSolution subject = new HelloSolution();
+
     @Test
     public void saysHelloToJohn() {
-        HelloSolution subject = new HelloSolution();
         String friendName = "John";
 
         String greeting = subject.hello(friendName);
@@ -19,13 +20,13 @@ public class HelloSolutionTest {
 
     @Test
     public void saysHelloToMike() {
-        HelloSolution subject = new HelloSolution();
         String friendName = "Mike";
 
         String greeting = subject.hello(friendName);
         assertThat(greeting, equalTo("Hello, " + friendName + "!"));
     }
 }
+
 
 
 
