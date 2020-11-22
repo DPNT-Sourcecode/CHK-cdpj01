fb0c567e45d30c9a198bb65a6e6ebec803f87f6d
sbx_user1051 <sbx_user1051@169.254.201.21>
2020-11-22 09:15:04 +0000

    Sun Nov 22 09:15:04 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,34 +1,26 @@
 package befaster.solutions.CHK;
 
-import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-
-
-
-
         return 50 * skus.length();
     }
 
-    private Map<Character, Integer> characterFrequencies(String skus) {
-        List<Character> characters = skus.chars()
+    /* package */ Map<Character, Long> characterFrequencies(String skus) {
+        Map<Character, Long> collect = skus.chars()
                 .mapToObj(c -> (char) c)
-
-                .collect(
-                        
-
+                .collect(Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.counting()
+                        )
                 );
-
-
-
     }
 
 }
+
 
 
 
