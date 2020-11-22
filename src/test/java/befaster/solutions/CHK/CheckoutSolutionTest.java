d9e24520e357094c4b4e44d1600fb4d8a936165c
sbx_user1051 <sbx_user1051@169.254.201.21>
2020-11-22 08:57:04 +0000

    Sun Nov 22 08:57:04 UTC 2020

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,18 @@
 package befaster.solutions.CHK;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
 public class CheckoutSolutionTest {
+
+
+    @Test
+    public void single_A_costs_50() {
+
+
+        Integer result;
+        assertThat(result, equalTo(50))
+    }
 }
+
