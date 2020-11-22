94e9cccf2ba4cc7ae19460738756f6f01c01288e
sbx_user1051 <sbx_user1051@169.254.201.21>
2020-11-22 09:00:04 +0000

    Sun Nov 22 09:00:04 UTC 2020

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -3,16 +3,33 @@ package befaster.solutions.CHK;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class CheckoutSolutionTest {
 
 
     @Test
     public void single_A_costs_50() {
-
-
-        Integer result;
-        assertThat(result, equalTo(50))
+        Integer result = new CheckoutSolution().checkout("A");
+        assertThat(result, equalTo(50));
     }
+
+    @Test
+    public void computes_cost_of_multiple_items_without_offers() {
+        Integer result = new CheckoutSolution().checkout("AA");
+        assertThat(result, equalTo(100));
+    }
+
+//    +------+-------+----------------+
+//    | Item | Price | Special offers |
+//    +------+-------+----------------+
+//    | A    | 50    | 3A for 130     |
+//    | B    | 30    | 2B for 45      |
+//    | C    | 20    |                |
+//    | D    | 15    |                |
+//    +------+-------+----------------+
+
 }
+
 
