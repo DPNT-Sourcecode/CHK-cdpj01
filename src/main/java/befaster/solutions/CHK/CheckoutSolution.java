8f9862f33fa5145a3cf0d20dec128f80f325bb2a
sbx_user1051 <sbx_user1051@169.254.225.117>
2020-11-22 12:16:39 +0000

    Sun Nov 22 12:16:39 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -17,7 +17,36 @@ public class CheckoutSolution {
                 new Offer("D", 15),
                 new Offer("E", 40),
                 new Offer("FFF", 20),
-                new Offer("F", 10)
+                new Offer("F", 10),
+                new Offer("G", 20),
+                new Offer("HHHHHHHHHH", 80),
+                new Offer("HHHHH", 45),
+                new Offer("I", 35),
+                new Offer("J", 60),
+                new Offer("KK", 150),
+                new Offer("K", 80),
+                new Offer("L", 90),
+                new Offer("NNNM", 120),
+                new Offer("N", 40),
+                new Offer("M", 15),
+                new Offer("O", 10),
+                new Offer("PPPPP", 200),
+                new Offer("P", 50),
+                new Offer("QQQ", 80),
+                new Offer("RRRQ", 150),
+                new Offer("R", 50),
+                new Offer("Q", 30),
+                new Offer("S", 30),
+                new Offer("T", 20),
+                new Offer("UUUU", 120),
+                new Offer("U", 40),
+                new Offer("VVV", 130),
+                new Offer("VV", 90),
+                new Offer("V", 50),
+                new Offer("W", 20),
+                new Offer("X", 90),
+                new Offer("Y", 10),
+                new Offer("Z", 50)
         );
 
         Basket basket = new Basket(skus);
@@ -35,6 +64,7 @@ public class CheckoutSolution {
         }
     }
 }
+
 
 
 
