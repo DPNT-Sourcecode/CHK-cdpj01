e95b64d55acff56c87b028821cd16bbc59c08aa0
sbx_user1051 <sbx_user1051@169.254.139.237>
2020-11-22 11:10:21 +0000

    Sun Nov 22 11:10:21 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,5 @@
 package befaster.solutions.CHK;
 
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
@@ -13,5 +9,6 @@ public class CheckoutSolution {
 
     }
 }
+
 
 
