af7989ca2170bff4580929ac032c11c969469f05
sbx_user1051 <sbx_user1051@169.254.233.109>
2020-11-23 19:15:47 +0000

    Mon Nov 23 19:15:47 UTC 2020

--- src/main/java/befaster/solutions/CHK/OffersBuilder.java
+++ src/main/java/befaster/solutions/CHK/OffersBuilder.java
@@ -2,6 +2,9 @@ package befaster.solutions.CHK;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class OffersBuilder {
 
@@ -11,12 +14,25 @@ public class OffersBuilder {
         return offers;
     }
 
-    OffersBuilder offer(String aaaaa, int i) {
-        offers.add(new Offer(aaaaa, i));
+    OffersBuilder offer(String skus, int price) {
+        offers.add(new Offer(skus, price));
+
+        return this;
+    }
+
+    OffersBuilder complexOffer(String skus, int numberPurchasedAtOnce, int price) {
+
+        Set<Character> skuSet = skus.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toSet());
+
+        skuSet.minus
+
 
         return this;
     }
 }
+
 
 
 
