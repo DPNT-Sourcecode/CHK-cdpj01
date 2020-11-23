09cae9e1da44a24a2adfafe4d9440e379d16fc57
sbx_user1051 <sbx_user1051@169.254.233.109>
2020-11-23 19:03:47 +0000

    Mon Nov 23 19:03:47 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,6 +1,5 @@
 package befaster.solutions.CHK;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class CheckoutSolution {
@@ -8,50 +7,7 @@ public class CheckoutSolution {
     public static final boolean LOGGING_ENABLED = false;
 
     public Integer checkout(String skus) {
-
-        List<Offer> offers = Arrays.asList(
-                new Offer("AAAAA", 200),
-                new Offer("AAA", 130),
-                new Offer("EEB", 80),
-                new Offer("BB", 45),
-                new Offer("A", 50),
-                new Offer("B", 30),
-                new Offer("C", 20),
-                new Offer("D", 15),
-                new Offer("E", 40),
-                new Offer("FFF", 20),
-                new Offer("F", 10),
-                new Offer("G", 20),
-                new Offer("HHHHHHHHHH", 80),
-                new Offer("HHHHH", 45),
-                new Offer("H", 10),
-                new Offer("I", 35),
-                new Offer("J", 60),
-                new Offer("KK", 120),
-                new Offer("K", 70),
-                new Offer("L", 90),
-                new Offer("NNNM", 120),
-                new Offer("N", 40),
-                new Offer("M", 15),
-                new Offer("O", 10),
-                new Offer("PPPPP", 200),
-                new Offer("P", 50),
-                new Offer("RRRQ", 150),
-                new Offer("R", 50),
-                new Offer("QQQ", 80),
-                new Offer("Q", 30),
-                new Offer("S", 20),
-                new Offer("T", 20),
-                new Offer("UUUU", 120),
-                new Offer("U", 40),
-                new Offer("VVV", 130),
-                new Offer("VV", 90),
-                new Offer("V", 50),
-                new Offer("W", 20),
-                new Offer("X", 17),
-                new Offer("Y", 20),
-                new Offer("Z", 21)
-        );
+        List<Offer> offers = new OffersBuilder().build();
 
         Basket basket = new Basket(skus);
 
@@ -75,6 +31,7 @@ public class CheckoutSolution {
         }
     }
 }
+
 
 
 
